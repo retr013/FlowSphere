7232ec8b99763b6373f85e3fd6cf1cb6c8ca004a
max <stagnirovat@gmail.com>
2025-05-13 23:26:35 +0100

    fix get function

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 import {prisma} from "@/prisma/client";
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest) {
     const users = await prisma.user.findMany({orderBy: {name: 'asc'}})
     return NextResponse.json(users)
 }
