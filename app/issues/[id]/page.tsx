97d237a903831ed4405dc222ee367626db341e27
max <stagnirovat@gmail.com>
2025-05-15 21:52:39 +0100

    some more fixes for vercel

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,3 +1,50 @@
+// import {prisma} from "@/prisma/client";
+// import {notFound} from "next/navigation";
+// import {Box, Flex} from "@radix-ui/themes";
+// import {IssueDetails} from "@/app/issues/[id]/IssueDetails";
+// import {UserSelector} from "@/app/issues/[id]/UserSelector";
+// import {StatusSelectorWrapper} from "@/app/issues/[id]/StatusSelectorWrapper";
+// import {Metadata} from "next";
+//
+// export const metadata: Metadata = {
+//     title: "Issue Details | FlowSphere",
+//     description: "View and edit the issue details",
+//     openGraph: {
+//         title: "Issue Details | FlowSphere",
+//         description: "View and edit the issue details",
+//         type: "website",
+//     }
+// }
+//
+// interface Props {
+//     params: { id: string };
+// }
+//
+// const IssueDetailPage = async ({params}: Props) => {
+//
+//     const id = parseInt(params.id as string);
+//
+//     const issue = await prisma.task.findUnique({
+//         where: {id},
+//     });
+//
+//     if (!issue) {
+//         notFound();
+//     }
+//
+//     return (
+//         <Box className="max-w-3xl mx-auto p-6 bg-bgdark text-white shadow-lg rounded-lg mt-16">
+//             <IssueDetails issue={issue}/>
+//             <Flex gap={'3'} className="mt-2">
+//                 <UserSelector issue={issue}/>
+//                 <StatusSelectorWrapper issue={issue} />
+//             </Flex>
+//         </Box>
+//     );
+// };
+//
+// export default IssueDetailPage;
+
 import {prisma} from "@/prisma/client";
 import {notFound} from "next/navigation";
 import {Box, Flex} from "@radix-ui/themes";
@@ -16,13 +63,14 @@ export const metadata: Metadata = {
     }
 }
 
-interface Props {
-    params: { id: string };
-}
+type Params = Promise<{ id: string }>
 
-const IssueDetailPage = async ({params}: Props) => {
 
-    const id = parseInt(params.id);
+const IssueDetailPage = async ({params}: {params: Params}) => {
+
+    const resolvedParams = await params;
+
+    const id = parseInt(resolvedParams.id as string);
 
     const issue = await prisma.task.findUnique({
         where: {id},
